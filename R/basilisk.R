1efb2098b34896232cc88e8a90659a82ef0b6b82
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-06-11 18:55:33 -0700

    Exported the dependency list for downstream use.

--- R/basilisk.R
+++ R/basilisk.R
@@ -1,4 +1,27 @@
-anndata_deps <- c(
+#' Define AnnData dependencies
+#'
+#' Define a versioned set of Python dependencies required to operate with AnnData and H5AD files.
+#'
+#' @details
+#' This variable is exposed for use by other package developers 
+#' who want an easy way to define the dependencies required for
+#' creating a Python environment to work with AnnData objects,
+#' most typically within a \pkg{basilisk} context.
+#' For example, we can simply combine this vector with additional
+#' dependencies to create a \pkg{basilisk} environment with Python
+#' package versions that are consistent with those in \pkg{zellkonverter}.
+#'
+#' @format
+#' A character vector containing the pinned versions of all Python packages on which AnnData depends.
+#'
+#' @author Luke Zappia
+#'
+#' @examples
+#' .AnnDataDependencies
+#' 
+#' @export
+#' @name AnnDataDependencies
+.AnnDataDependencies <- c(
     "anndata==0.7.3",
     "h5py==2.10.0",
     "hdf5==1.10.5",
@@ -13,5 +36,5 @@ anndata_deps <- c(
 anndata_env <- basilisk::BasiliskEnvironment(
     envname="anndata_env",
     pkgname="zellkonverter",
-    packages=anndata_deps
+    packages=.AnnDataDependencies
 )
