dd7891d6c2257f0cbc7a24150c24f1ebe83563ea
Luke Zappia <lazappi@users.noreply.github.com>
2021-01-30 15:53:53 +0100

    Add DelayedArray tests for writeH5AD

--- tests/testthat/test-write.R
+++ tests/testthat/test-write.R
@@ -2,6 +2,7 @@
 # library(testthat); library(zellkonverter); source("test-write.R")
 
 library(scRNAseq)
+
 sce <- ZeiselBrainData()
 reducedDim(sce, "WHEE") <- matrix(runif(ncol(sce) * 10), ncol = 10)
 
@@ -84,4 +85,38 @@ test_that("writeH5AD works in a separate process", {
 
     basilisk::setBasiliskShared(oldshare)
     basilisk::setBasiliskFork(oldfork)
+})
+
+test_that("writeH5AD DelayedArray X works", {
+
+    delayed_sce <- sce
+    counts(delayed_sce) <- DelayedArray::DelayedArray(counts(delayed_sce))
+
+    temp <- tempfile(fileext = '.h5ad')
+
+    writeH5AD(delayed_sce, temp, X_name = "counts")
+    expect_true(file.exists(temp))
+
+    out <- readH5AD(temp)
+
+    expect_identical(counts(sce), assay(out, "X"))
+
+})
+
+test_that("writeH5AD DelayedArray layer works", {
+
+    delayed_sce <- sce
+    assay(delayed_sce, "layer") <- DelayedArray::DelayedArray(
+        counts(delayed_sce)
+    )
+
+    temp <- tempfile(fileext = '.h5ad')
+
+    writeH5AD(delayed_sce, temp)
+    expect_true(file.exists(temp))
+
+    out <- readH5AD(temp)
+
+    expect_identical(counts(sce), assay(out, "layer"))
+
 })
