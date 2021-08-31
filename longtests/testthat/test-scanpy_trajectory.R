3adf7809f14fd2a5682438ff10cc4f3ce965060f
Luke Zappia <lazappi@users.noreply.github.com>
2021-08-31 15:31:58 +0200

    Update scanpy_trajectory datasets
    Now includes diffmap results

--- longtests/testthat/test-scanpy_trajectory.R
+++ longtests/testthat/test-scanpy_trajectory.R
@@ -2,16 +2,16 @@ library(SingleCellExperiment)
 library(BiocFileCache)
 
 cache <- BiocFileCache(ask = FALSE)
-file <- bfcrpath(cache, "https://figshare.com/ndownloader/files/30578574")
+file <- bfcrpath(cache, "https://figshare.com/ndownloader/files/30594477")
 outfile <- tempfile(fileext = ".h5ad")
 
 names <- list(
     assays = c("X"),
-    colData = c("paul15_clusters", "n_counts_all", "louvain"),
+    colData = c("paul15_clusters", "n_counts_all", "louvain", "dpt_pseudotime"),
     rowData = c("n_counts", "mean", "std"),
-    metadata = c("draw_graph", "iroot", "louvain", "louvain_sizes", "neighbors",
-                 "paga", "pca"),
-    redDim = c("X_draw_graph_fa", "X_pca"),
+    metadata = c("diffmap_evals", "draw_graph", "iroot", "louvain",
+                 "louvain_sizes", "neighbors", "paga", "pca"),
+    redDim = c("X_diffmap", "X_draw_graph_fa", "X_pca"),
     varm = c("PCs"),
     colPairs = c("connectivities", "distances")
 )
